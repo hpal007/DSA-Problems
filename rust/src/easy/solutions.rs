264518802d95b945de63ba1c0d14847401e9a269
Harishchandra Pal <harishchandra.pal@outlook.com>
2024-12-01 20:49:09 +0530

    Day01

--- rust/src/easy/solutions.rs
+++ rust/src/easy/solutions.rs
@@ -1,4 +1,6 @@
-// Solution for problem 1: Transpose of 
+
+
+// Solution for problem 1: Transpose of
 pub fn transpose(matrix: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
     let r = matrix.len();
     let c = matrix[0].len();
@@ -15,12 +17,39 @@ pub fn transpose(matrix: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
 }
 
 // Solution for problem 2: Square of sorted arrays
-pub fn sorted_squares(nums: Vec<i32>) -> Vec<i32> {
-    let mut output: Vec<i32> = Vec::new();
-    for num in nums {
-        output.push(num * num)
+pub fn sorted_squares_1(nums: Vec<i32>) -> Vec<i32> {
+    // simple way to do it with sort. 
+    let mut output:Vec<i32> = Vec::new();
+    for num in nums{
+        output.push(num*num);
     }
-
     output.sort();
+    output
+}
+
+pub fn sorted_squares(nums: Vec<i32>) -> Vec<i32> {
+    let num_length = nums.len();
+    let mut output: Vec<i32> = vec![0; num_length];
+
+    let mut right = num_length - 1;
+    let mut left = 0;
+    let mut index = num_length - 1;
+
+    while left <= right {
+        let left_square = nums[left] * nums[left];
+        let right_square = nums[right] * nums[right];
+
+        if right_square > left_square {
+            output[index] = right_square;
+            right -= 1;
+        } else {
+            output[index] = left_square;
+            left += 1;
+        }
+        
+        index = index.saturating_sub(1); // because using index -=1 gives overflow err, this stops that warning selecting mininum value only not overflow.
+        
+    }
+    
     output
 }
